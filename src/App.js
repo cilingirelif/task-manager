b3099283e7c2c13d95e736f7a75fd23c5170bf0a
Elif Cilingir <cilingir43@gmail.com>
2020-03-07 13:29:46 +0300

    Add project page to the route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import history from "./history.js";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import AddProject from "./screens/AddProject";
+import Project from "./screens/Project";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" component={Login} exact={true} />
         <Route path="/home"  render={props => <Home {...props} />} exact={true} />
         <Route path="/add-project"  render={props => <AddProject {...props} />} exact={true} />
+        <Route path="/project/:id"  render={props => <Project {...props} />} exact={true} />
       </Router>
     </div>
   );
