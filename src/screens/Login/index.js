c8a7aa5c5bbc318ea3a3219f08604804a468900d
Elif Cilingir <cilingir43@gmail.com>
2020-03-08 00:04:33 +0300

    Update login form
    - Added simple validation form

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,32 +1,67 @@
 import React, { useState } from "react";
 import 'antd/dist/antd.css';
 import '../../App.css';
-import { Link } from "react-router-dom/";
+import history from "../../history";
 
 function Login(){
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState(false);
+    const [emailErrorMessage] = useState("Email can't be blank!");
     const [password, setPassword] = useState("");
-   
-    //   const handleSubmit = e => {
-    //     e.preventDefault();}
+    const [passwordError, setPasswordError] = useState(false);
+    const [passwordErrorMessage] = useState("Password can't be blank!");
+
+    const resetErrors = () => {
+        setEmailError(false);
+        setPasswordError(false);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        resetErrors();
+
+        if (!email) {
+            setEmailError(true);
+        }
+        if (!password) {
+            setPasswordError(true);
+        }
+        if (!email || !password) {
+            return;
+        }
+        history.push("home");
+    }
+
+    
     return(
     <div className="container">    
         <div className="login_container" >
             <div className="wrap-login flex column">
                 <div className="title">Task Manager</div>
                 <form >
+                    <div className="flex row align-center">
+                          <div className="error text-align-end width100">
+                            {emailError && emailErrorMessage}   
+                          </div>
+                    </div>
                     <div className="flex row align-center mb-20">
                         <div className="label">Email</div>
 
                         <input
                             className="input_text"
                             type="text"
-                            placeholder="Your e-mail"
                             name="email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                         />
 
+                    </div>
+                    <div className="flex row align-center">
+                        {passwordError &&
+                          <div className="error text-align-end width100">
+                            {passwordErrorMessage}
+                          </div>
+                        }
                     </div>
                     <div className="flex row align-center mb-20">
                         <div className="label">Password</div>
@@ -34,7 +69,6 @@ function Login(){
                         <input
                             className="input_text"
                             type="password"
-                            placeholder="Your password"
                             name="password"
                             value={password}
                             onChange={e => setPassword(e.target.value)}
@@ -42,7 +76,8 @@ function Login(){
 
                     </div>
                     <div className="flex justify-center mb-20">
-                        <Link to="/home" className="btn" >Login</Link>
+
+                    <button type="primary" className="btn" onClick={handleSubmit} >Login</button>
                     </div>
            
                 </form>
