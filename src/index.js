449a265ae8d8c9110551631b0b0ff0aeeac79ca5
Elif Cilingir <cilingir43@gmail.com>
2020-03-07 13:04:28 +0300

    Integrate redux-persist & remote-redux-devtools to redux store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
-import store from './redux/store'
+import { store } from './redux/store'
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,37 @@
-import {createStore} from "redux"
+import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import { composeWithDevTools } from "remote-redux-devtools";
+import storage from "redux-persist/lib/storage";
+
 import rootReducer from "./reducers"
 
-export default createStore(rootReducer)
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: []
+  };
+
+const makeStore = () => {
+    const middleware = [];
+    const enhancers = [];
+  
+    enhancers.push(applyMiddleware(...middleware));
+  
+    // Redux persist
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+  
+    // Redux devtools
+    const composeEnhancers = composeWithDevTools({
+      hostname: "localhost",
+      port: 8000
+    });
+  
+    const store = createStore(persistedReducer, composeEnhancers(...enhancers));
+    const persistor = persistStore(store);
+  
+    return { store, persistor };
+  };
+  
+  const { store, persistor } = makeStore();
+  
+  export { store, persistor };
