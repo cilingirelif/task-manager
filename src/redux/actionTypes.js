3102ad9b647a75cd2d26b40d87681dcfe8d7dbd4
Elif Cilingir <cilingir43@gmail.com>
2020-03-08 22:02:24 +0300

    Update Project Detail Screen
    - Remove/Rename UpdateTaskStatus -> UpdateTask
    - Add DeleteTask
    - Add ant-d responsive grid
    - Add row selection
    - Add project style file

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,3 +1,4 @@
 export const ADD_PROJECT = "ADD_PROJECT";
 export const ADD_TASK = "ADD_TASK";
 export const UPDATE_TASK = "UPDATE_TASK"
+export const DELETE_TASK = "DELETE_TASK";
--- src/screens/Project/index.js
+++ src/screens/Project/index.js
@@ -1,133 +1,235 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom/";
 import { connect } from "react-redux";
-import { Button, notification, Table, Tag, Modal,Select } from 'antd';
-import 'antd/dist/antd.css';
-import '../../App.css';
-import { updateTaskStatus } from "../../redux/actions";
-import Navbar from "../../components/Navbar"
+import {
+  Button,
+  notification,
+  Table,
+  Tag,
+  Modal,
+  Select,
+  Row,
+  Col,
+  Form,
+  Input,
+  Popconfirm
+} from "antd";
+import "antd/dist/antd.css";
+import "../../App.css";
+import "./project.css";
+import { updateTask, deleteTask } from "../../redux/actions";
+import Navbar from "../../components/Navbar";
 
 //Request
-import updateTaskStatusRequest from "../../api/updateTaskStatusRequest";
 import CreateTask from "../../components/CreateTask.js";
+import updateTaskRequest from "../../api/updateTaskRequest";
+import deleteTaskRequest from "../../api/deleteTaskRequest";
 
-function Project(props){
-  const { project, tasks, updateTaskStatus } = props
+function Project(props) {
+  const { project, tasks, updateTask, deleteTask } = props;
 
   const [filteredInfo, setFilteredInfo] = useState({});
-  const [sortedInfo, setSorteredInfo] = useState({});
+  const [sortedInfo, setSorteredInfo] = useState({
+    columnKey: "id",
+    order: "descend"
+  });
   const [visible, setVisible] = useState(false);
-  const [selectValue, setSelectValue] = useState("");
-  const [taskId, setTaskId] = useState("");
+  const [selectedTask, setSelectedTask] = useState("");
+  const [selectedStatus, setSelectedStatus] = useState("");
+  const [selectedTaskId, setSelectedTaskId] = useState("");
 
   const { Option } = Select;
 
-  const handleTag = (record) => {
+  const handleSelection = record => {
     setVisible(true);
-    setTaskId(record.id);
-    setSelectValue(record.status);
-  }
-
-  const handleTableChange = (pagination, filters, sorter) => {
-    setFilteredInfo(filters)
-    setSorteredInfo(sorter)
+    setSelectedTaskId(record.id);
+    setSelectedTask(record.task);
+    setSelectedStatus(record.status);
   };
 
-  const handleSelectChange = (value)=>{
-    setSelectValue(value)
-  }
+  const handleTableChange = (pagination, filters, sorter) => {
+    setFilteredInfo(filters);
+    setSorteredInfo(sorter);
+  };
+
+  const handleSelectChange = value => {
+    setSelectedStatus(value);
+  };
 
   let columns = [
     {
-      title: 'Id',
-      dataIndex: 'id',
-      key: 'id',
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
       sorter: (a, b) => a.id - b.id,
-      sortOrder: sortedInfo.columnKey === 'id' && sortedInfo.order,
+      sortOrder: sortedInfo.columnKey === "id" && sortedInfo.order,
       ellipsis: true,
+      align: "center"
     },
     {
-      title: 'Task',
-      dataIndex: 'task',
-      key: 'task',
+      title: "Task",
+      dataIndex: "task",
+      key: "task"
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
       filters: [
-        { text: 'Todo', value: 'Todo' },
-        { text: 'Done', value: 'Done' },
+        { text: "Todo", value: "Todo" },
+        { text: "Done", value: "Done" }
       ],
       filteredValue: filteredInfo.status || null,
       onFilter: (value, record) => record.status.includes(value),
-      render: (status,record) => (
+      render: (status, record) => (
         <span>
           {
-            <Tag onClick={()=>handleTag(record)} color={status ==="Todo" ? 'volcano' : 'green'} key={status}>
+            <Tag color={status === "Todo" ? "volcano" : "green"} key={status}>
               {status}
             </Tag>
           }
         </span>
-      ),
-    },
+      )
+    }
   ];
 
-  const handleUpdateTaskStatus = () => {
-    setVisible(false)
+  const handleDeleteTask = () => {
     const data = {
-      id: taskId,
-      status: selectValue
-    }
-    
-    updateTaskStatusRequest(data).then(response => {
+      id: selectedTaskId
+    };
+
+    deleteTaskRequest(data).then(response => {
+      setVisible(false);
       if (response.status === "success") {
-        updateTaskStatus(response.data); //update store with response
+        deleteTask(response.data); //update store with response
         notification.success({
           message: "Successful!",
-          description: (
-            <>
-              {"Updated successfully."}
-            </>
-          )
+          description: <>{"Deleted successfully."}</>
         });
-       
       } else {
         notification.error({ message: "Something went wrong" });
       }
     });
-  }
+  };
 
-  
-  return(
+  const handleUpdateTask = () => {
+    setVisible(false);
+    const data = {
+      id: selectedTaskId,
+      status: selectedStatus,
+      task: selectedTask
+    };
+
+    updateTaskRequest(data).then(response => {
+      if (response.status === "success") {
+        updateTask(response.data); //update store with response
+        notification.success({
+          message: "Successful!",
+          description: <>{"Updated successfully."}</>
+        });
+      } else {
+        notification.error({ message: "Something went wrong" });
+      }
+    });
+  };
+
+  return (
     <div>
       <Navbar />
-      <div className="container">    
-        <div className="wrap-login flex column">
-            {project &&
-              <div className="title">{project.name}</div>
-            }
+      <div className="container project-container">
+        <Row>
+          <Col className="project-detail-column" span={24} xl={7}>
+            {project && (
+              <>
+                <div className="project-detail-title">{project.name}</div>
+                <p className="project-detail-description">
+                  {project.description}
+                </p>
+              </>
+            )}
+
             <CreateTask project={project} />
-            <Table rowKey={"id"} columns={columns} dataSource={tasks} onChange={handleTableChange} />
+          </Col>
+          <Col className="project-detail-column" span={24} xl={17}>
+            <Table
+              tableLayout="auto"
+              rowKey={"id"}
+              columns={columns}
+              dataSource={tasks}
+              onChange={handleTableChange}
+              onRow={record => ({
+                onClick: () => {
+                  handleSelection(record);
+                }
+              })}
+              pagination={{
+                pageSize: 8
+              }}
+            />
             <Modal
-              title="Change status"
+              title="Edit Task"
               visible={visible}
-              onOk={handleUpdateTaskStatus}
+              onOk={handleUpdateTask}
               onCancel={() => setVisible(false)}
+              footer={[
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Popconfirm
+                    title={"Are you sure to delete this task?"}
+                    onConfirm={() => handleDeleteTask()}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <Button type="danger">Delete</Button>
+                  </Popconfirm>
+                  <div>
+                    <Button key="back" onClick={() => setVisible(false)}>
+                      Cancel
+                    </Button>
+                    <Button
+                      key="submit"
+                      type="primary"
+                      onClick={handleUpdateTask}
+                    >
+                      Update
+                    </Button>
+                  </div>
+                </div>
+              ]}
             >
-                <Select style={{ width: 120 }} value={selectValue} onChange={handleSelectChange}>
-                   <Option value="Todo">Todo</Option>
-                   <Option value="Done">Done</Option>
-                </Select>
+              <Form layout="vertical">
+                <Form.Item label="Task">
+                  <Input.TextArea
+                    type="text"
+                    placeholder="Create new task"
+                    name="task"
+                    autoSize
+                    value={selectedTask}
+                    onChange={e => setSelectedTask(e.target.value)}
+                    onPressEnter={handleUpdateTask}
+                  />
+                </Form.Item>
+                <Form.Item label="Status">
+                  <Select
+                    style={{ width: 120 }}
+                    value={selectedStatus}
+                    onChange={handleSelectChange}
+                  >
+                    <Option value="Todo">Todo</Option>
+                    <Option value="Done">Done</Option>
+                  </Select>
+                </Form.Item>
+              </Form>
             </Modal>
-        </div>
+          </Col>
+        </Row>
       </div>
     </div>
-    )
+  );
 }
 
-const mapStateToProps = (state,ownProps ) => {
-  let projectId = ownProps.match.params.id
+const mapStateToProps = (state, ownProps) => {
+  let projectId = ownProps.match.params.id;
   return {
     project: state.projects.list.filter(
       project => project.id === parseInt(projectId)
@@ -136,5 +238,5 @@ const mapStateToProps = (state,ownProps ) => {
       task => task.projectId === parseInt(projectId)
     )
   };
-}; 
-export default connect(mapStateToProps, {  updateTaskStatus })(Project)
+};
+export default connect(mapStateToProps, { updateTask, deleteTask })(Project);
