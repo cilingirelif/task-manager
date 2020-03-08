567b543e3fc124915bf4eb8edcb28627710f4471
Elif Cilingir <cilingir43@gmail.com>
2020-03-08 21:10:38 +0300

    Update Add Project screen view

--- src/screens/AddProject/index.js
+++ src/screens/AddProject/index.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom/";
 import 'antd/dist/antd.css';
 import '../../App.css';
-import {
-  HomeOutlined,UserOutlined,PlusOutlined
-} from '@ant-design/icons';
-import { Menu, Button, notification } from 'antd';
+
+import { Button, notification} from 'antd';
 import { connect } from "react-redux";
 import { addProject } from "../../redux/actions";
 import Navbar from "../../components/Navbar"
@@ -86,57 +83,59 @@ function AddProject(props){
     <div>
 
       <Navbar />
-      <div className="container">    
-  
-        <div className="wrap-login flex column">
-          <div className="title">Add project</div>
-            <form>
-              <div className="flex row align-center">
-                {projectNameError &&
-                  <div className="error text-align-end width100">
-                    {projectNameErrorMessage}
-                  </div>
-                }
-              </div>
+      <div className="container">
+        <div className="wrapper">
+          <div className=" flex column">
+            <div className="title">Add project</div>
+            <div className="line mb-20"></div>
+       
+            <form className="add-project-form">
+                <div className="flex row align-center">
+                  {projectNameError &&
+                    <div className="error text-align-end width100">
+                      {projectNameErrorMessage}
+                    </div>
+                  }
+                </div>
 
-              <div className="flex row align-center mb-20">
-                <div className="label">Project Name</div>
-                <input
-                    className="input_text"
+                <div className="flex row align-center mb-20">
+                  <div className="label">Project Name</div>
+                  <input
+                      className="input_text"
+                      type="text"
+                      placeholder=""
+                      name="project_name"
+                      value={projectName}
+                      onChange={e => setProjectName(e.target.value)}
+                  />
+                </div>
+                <div className="flex row align-center">
+                  {descriptionError &&
+                    <div className="error text-align-end width100">
+                      {descriptionErrorMessage}
+                    </div>
+                  }
+                </div>
+                <div className="flex row align-center mb-20">
+                  <div className="label">Description</div>
+                  <textarea
+                    rows="6"
+                    className="textarea_text"
                     type="text"
                     placeholder=""
-                    name="project_name"
-                    value={projectName}
-                    onChange={e => setProjectName(e.target.value)}
-                />
-              </div>
-              <div className="flex row align-center">
-                {descriptionError &&
-                  <div className="error text-align-end width100">
-                    {descriptionErrorMessage}
-                  </div>
-                }
-              </div>
-              <div className="flex row align-center mb-20">
-                <div className="label">Description</div>
-
-                <textarea
-                  rows="6"
-                  className="textarea_text"
-                  type="text"
-                  placeholder=""
-                  name="description"
-                  value={description}
-                  onChange={e => setDescription(e.target.value)}
-                />
-              </div>
-
-              <Button type="primary" loading={isLoading} onClick={handleSubmit}>Save</Button>
-            
-            </form>
+                    name="description"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-end">
+                  <Button type="primary" loading={isLoading} onClick={handleSubmit}>Save</Button>
+                </div>
+              </form>
+            </div>
           </div>
+        </div>
       </div>
-    </div>
     )
 }
 export default connect(null,{ addProject })(AddProject)
